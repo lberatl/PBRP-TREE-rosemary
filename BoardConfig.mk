9e193301fa17fc5385b6363d4683932bd836d7ff
Berat <75970161+BerseusBerat@users.noreply.github.com>
2024-03-18 00:34:09 +0300

    Update BoardConfig.mk
    flashlight fix

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,6 +167,6 @@ RECOVERY_BINARY_SOURCE_FILES += $(TARGET_OUT_EXECUTABLES)/hw/android.hardware.vi
 
 #For_PB
 PB_DISABLE_DEFAULT_DM_VERITY := true
-PB_TORCH_PATH := "/sys/class/leds/./brightness"
+PB_TORCH_PATH := /sys/class/backlight/panel0-backlight/brightness
 
 
